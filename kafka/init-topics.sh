864e648de47ab8f22711bedf5650313d05db616f
Constantin Stanca <cstanca@Constantins-New-MacBook-Pro.local>
2026-03-07 21:34:48 -0600

    Add kafka-init bootstrap service

--- kafka/init-topics.sh
+++ kafka/init-topics.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+set -e
+
 sleep 15
-kafka-topics 
---create 
---topic app_logs 
---bootstrap-server kafka:9092 
---partitions 3 
---replication-factor 1
+
+kafka-topics \
+  --create \
+  --if-not-exists \
+  --topic app_logs \
+  --bootstrap-server kafka:9092 \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Kafka topic app_logs created"
